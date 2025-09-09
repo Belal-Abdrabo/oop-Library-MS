9da24e1c3da4859316ae804dce018592d8630878
Belal-Abdrabo <belalabdoo959@gmail.com>
2025-09-10 02:54:15 +0300

    none

---
+++ OOP-library-management-system/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_library_management_system
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
